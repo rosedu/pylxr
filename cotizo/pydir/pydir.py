f5131ce3b17a1347d0b42bf51a68593e7931dc46
Cotizo Sima <kasei90@yahoo.com>
2010-07-08 13:09:38 +0300

    Added pydir : walks through a directory and outputs the size of the inspected directories

---
+++ cotizo/pydir/pydir.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from datetime import datetime
+from string import ljust
+from stat import *
+
+def mywalk(path, depth, maxd):
+    if depth==maxd:
+        return 0
+    
+    try:
+        content = os.listdir(path)
+    except Exception as err:
+        print err
+        return 0
+
+    s = 0
+        
+    for item in content:
+        fullpath = os.path.join(path, item)
+        print ljust(fullpath, 50),
+        try:
+            mode = os.stat(fullpath)[ST_MODE]
+            if S_ISDIR(mode):
+                print "Directory"
+                m = mywalk(fullpath, depth+1, maxd)
+                print "Directory size: ", m
+                s = s+m
+            elif S_ISREG(mode):
+                print "Regular file\t%s\t%s" % (os.stat(fullpath)[ST_SIZE], datetime.fromtimestamp(os.stat(fullpath)[ST_MTIME]))
+                s = s+int(os.stat(fullpath)[ST_SIZE])
+            elif S_ISLNK(mode):
+                print "Link"
+            elif S_ISSOCK(mode):
+                print "Socket"
+            elif S_ISFIFO(mode):
+                print "Pipe"
+
+        except Exception as err:
+            print err
+
+    return s
+
+            
+
+def main(argv=None):
+    if argv==None:
+        argv = sys.argv[1:]
+    maxd = int(argv[1])
+    s = mywalk(argv[0], 0, maxd)
+    print "Total size: ", s
+
+if __name__=='__main__':
+    sys.exit(main())
